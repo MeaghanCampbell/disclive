0cf8fd60009f17b02d5ef7dc4556cab46773e1c7
Meaghan Campbell <emeaghancampbell@gmail.com>
2023-09-19 11:59:48 -0400

    move files

---
+++ README.md
@@ -0,0 +1,43 @@
+# DISClive
+
+
+## 💬Description
+DISClive is a live music discovery platform. Users can search artists they love, find artists that are similar, and see if any of those similar artists are playing shows near them. 
+
+
+## Purpose 
+* AS A lover of live music
+* I WANT to see new artists in concert
+* SO THAT I can expand my music repitiore and find artists to follow that are similar to the ones I like
+<br></br>
+<br></br>
+* GIVEN ...
+* WHEN I ...
+* THEN I ...
+
+
+## 🎨Mock-Up
+<img width="421" alt="Screen Shot 2021-02-01 at 2 29 35 PM" src="https://user-images.githubusercontent.com/74511935/106514574-08c80700-649a-11eb-8080-aa3013cb1c7d.png">
+<img width="422" alt="Screen Shot 2021-02-01 at 2 29 40 PM" src="https://user-images.githubusercontent.com/74511935/106514589-0ebde800-649a-11eb-8962-0d7d5eebb6a7.png">
+
+
+## Built With
+* HTML
+* CSS
+* Javascript
+* Tailwind
+* Google Fonts
+* Font Awesome
+* TasteDive API
+* BandsInTown API
+
+
+## 💻Website
+https://meaghancampbell.github.io/disc-live/
+
+<br>![](website.gif)</br>
+
+
+## 📌Contributors
+Meaghan Campbell, Taylor Gentry, Daniel Starkey
+
---
+++ assets/js/script.js
@@ -0,0 +1,158 @@
+// select search button
+var searchBtnEl = document.querySelector('#search-btn')
+
+// select search input
+var searchInputEl = document.querySelector('#search-input')
+
+// select artist container
+var artistContainerEl = document.querySelector('#similar-artist-container')
+
+// find shows button
+var showsBtnEl = document.createElement('button')
+
+// target recent searches button
+var recentSearchEl = document.querySelector('#recent-searches')
+
+//select recent searched artist
+var recentSearch = localStorage.getItem ("storedArtist")
+
+// function to run recent searches
+var searchRecentFunction = function () {
+  if (recentSearch) {
+    artistName = localStorage.getItem ("storedArtist")
+    fetchTasteData(artistName);
+  
+  }
+}
+
+// search function
+var submitSearch = function(event) {
+    event.preventDefault();
+    artistContainerEl.innerHTML = '';
+    
+    // get value from artist name input
+    var artistName = searchInputEl.value.trim();
+
+    if (artistName) {
+        fetchTasteData(artistName);
+        searchInputEl.value = "";
+
+        // store input to local storage
+        localStorage.setItem("storedArtist", artistName);
+      
+    } 
+}
+
+
+// search by enter btn
+var pressEnter = document.getElementById('search-input');
+pressEnter.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById('search-btn').click();
+  }
+});
+
+
+var fetchTasteData = function(artistName) {
+  fetch(
+    `http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=${artistName}&api_key=b370f92f2a49f6e2167f2bdd9da3c14b&format=json`
+  )
+  .then(function(response) {
+
+      return response.json();
+    
+  })
+  .then(function(data) {
+
+      let artistArray = data.similarartists.artist
+
+      console.log(artistArray)
+      
+      displaySongPlayer(artistArray)
+
+  })
+}
+
+
+// function to display artist
+var displaySongPlayer = function(data) {
+
+    var artistNameDisplay = document.querySelector('#searched-artist')
+    artistNameDisplay.textContent = ''
+
+    for (let i = 0; i < 5; i++) {
+
+    // create artist background (li)
+    var artistBackgroundEl = document.createElement('li')
+    artistBackgroundEl.className = 'artist-background'
+    artistBackgroundEl.setAttribute('id', 'container-' + i)
+
+    // create p element and give it value of songTitle (searched term)
+    var artistNameEl = document.createElement('p')
+    artistNameEl.classList.add('song-details')
+    artistNameEl.textContent = data[i].name
+
+    // create container for buttons
+    var btnContainerEl = document.createElement('div')
+    btnContainerEl.classList.add('button-container')
+
+    // create find shows button
+    var showsBtnEl = document.createElement('a')
+    showsBtnEl.classList.add('shows')
+    showsBtnEl.textContent = 'Find Shows'
+    showsBtnEl.setAttribute('href', './playPage.html?q=' + artistNameEl.textContent);
+
+    // create trash button and icon
+    var trashBtnEl = document.createElement('button')
+    trashBtnEl.classList.add('trash')
+    trashBtnEl.classList.add('far')
+    trashBtnEl.classList.add('fa-trash-alt')
+    // id for trash button so we can target to remove item
+    trashBtnEl.setAttribute('id', 'button-' + i)
+    trashBtnEl.addEventListener('click', function(e) {
+      removeArtist(e, i)
+    })
+
+    // append elements to page
+    artistContainerEl.appendChild(artistBackgroundEl)
+    artistBackgroundEl.appendChild(artistNameEl)
+    artistBackgroundEl.appendChild(btnContainerEl)
+    btnContainerEl.appendChild(showsBtnEl)
+    btnContainerEl.appendChild(trashBtnEl)
+
+    }
+
+}
+
+var removeArtist = function(event, i) {
+
+event.stopPropagation()
+var element = document.getElementById('container-' + i)
+var button = document.getElementById('button-' + i)
+
+if (event.target === button) {
+  element.remove()
+}
+
+}
+
+
+// listen for search button click
+searchBtnEl.addEventListener('click', submitSearch);
+
+// listen for trash button click
+artistContainerEl.addEventListener('click', removeArtist)
+
+//listen for recent searches button click
+recentSearchEl.addEventListener('click', searchRecentFunction)
+
+
+
+
+
+
+
+
+
+
